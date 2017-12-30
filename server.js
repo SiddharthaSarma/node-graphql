f488371e999a27f3897a5dfd04d876b837efc111
siddharthasarma <sidduinevitable@gmail.com>
2017-12-30 09:42:14 +0000

    Formatted the code.

--- server.js
+++ server.js
@@ -4,10 +4,13 @@ const schema = require('./schema');
 const port = 4000;
 const app = express();
 
-app.use('/graphql', expressGraphql({
-  schema: schema,
-  graphiql: true
-}));
+app.use(
+  '/graphql',
+  expressGraphql({
+    schema: schema,
+    graphiql: true
+  })
+);
 
 app.listen(process.env.PORT || port, () => {
   console.log('listening....');
