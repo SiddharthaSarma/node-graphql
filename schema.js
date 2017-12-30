8b3d4ecc832341d2aca0f28e9c548dd285421bcb
siddharthasarma <sidduinevitable@gmail.com>
2017-12-30 09:10:16 +0000

    Updated code in schema file.

--- schema.js
+++ schema.js
@@ -1,12 +1,35 @@
 const {
-    GraphQLObjectType,
-    GraphQLString,
-    GraphQLInt,
-    GraphQLSchema,
-    GraphQLList,
-    GraphQLNonNull
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+  GraphQLList,
+  GraphQLNonNull
 } = require('graphql');
 
+const CustomerType = new GraphQLObjectType({
+  name: 'Customer',
+  fields: () => ({
+    id: {type: GraphQLString},
+    name: {type.GraphQLString},
+    email: {type.GraphQLString},
+    age: {type.GraphQLInt}
+  })
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQuery',
+  customer: {
+    type: CustomerType,
+    args: {
+      id: {type: GraphQLString}
+    },
+    resolve(parentValue, args) {
+      
+    }
+  }
+})
+
 module.exports = new GraphQLSchema({
-    
+    query: RootQuery
 });
