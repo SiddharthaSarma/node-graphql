e339c490bef9a2317d9bd336774d569d3e74e952
siddharthasarma <sidduinevitable@gmail.com>
2018-06-15 19:52:17 +0530

    create singleton logger instance

---
+++ src/logger/index.js
@@ -0,0 +1,18 @@
+import { format, createLogger, transports } from 'winston';
+const { timestamp, combine, prettyPrint } = format;
+
+class Logger {
+  constructor() {
+    this.logger = createLogger({
+      format: combine(timestamp(), prettyPrint()),
+      transports: [
+        new transports.File({ filename: 'error.log', level: 'error' }),
+        new transports.File({ filename: 'combined.log' })
+      ]
+    });
+  }
+  log(level, msg, ...meta) {
+    this.logger.log(level, msg, ...meta);
+  }
+}
+export default new Logger();
