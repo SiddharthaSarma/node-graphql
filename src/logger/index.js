ad5529254c8598bb5386eb5fd2dae527a79ebb6c
siddharthasarma <sidduinevitable@gmail.com>
2018-06-18 05:40:10 +0000

    Moved logs to separate folder

--- src/logger/index.js
+++ src/logger/index.js
@@ -1,13 +1,22 @@
 import { format, createLogger, transports } from 'winston';
+import fs from 'fs';
+import path from 'path';
+
 const { timestamp, combine, prettyPrint } = format;
+const logsDir = 'logs';
+
+// create logs directory if it doesn't exist
+if (!fs.existsSync(logsDir)) {
+  fs.mkdirSync(logsDir);
+}
 
 class Logger {
   constructor() {
     this.logger = createLogger({
       format: combine(timestamp(), prettyPrint()),
       transports: [
-        new transports.File({ filename: 'error.log', level: 'error' }),
-        new transports.File({ filename: 'combined.log' })
+        new transports.File({ filename: path.join(logsDir, 'error.log'), level: 'error' }),
+        new transports.File({ filename: path.join(logsDir, 'combined.log') })
       ]
     });
   }
